d63663f9e8af70b4e20d88fba72a16a6a37a8c84
liuhp <liuhp@jackyun.com>
2021-07-24 15:42:56 +0800

    second commit

---
+++ src/test/java/com/pokors/utils/demo/UtilsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokors.utils.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
